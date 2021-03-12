a86dd2b73c7f5c1510acc9a2425b69baf3c6ad02
Camilo Paez <cepn26@gmail.com>
2021-03-11 23:41:47 -0500

    :sparkles: First search in tests

--- src/pages/CharactersPage.js
+++ src/pages/CharactersPage.js
@@ -5,11 +5,12 @@ import useObserver from '../hooks/useObserver'
 export default function CharactersPage() {
   const ref = useRef(null)
 
-  const [page, setPage] = useState(-1) //A BUG THAT I CAN UNDERSTAND
+  const [page, setPage] = useState(-1) //A BUG THAT I CANT UNDERSTAND
   const isIntersecting = useObserver(ref)
 
   useEffect(() => {
     console.log(isIntersecting)
+    // TODO: Arreglar bug
     if (isIntersecting) {
       setPage(page + 1)
     }
--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,20 +1,27 @@
 import qs from 'querystring'
-import { useRef } from 'react'
-import ListOfCharacters from '../components/ListOfCharacters'
-import useObserver from '../hooks/useObserver'
+// import { useRef } from 'react'
+// import useObserver from '../hooks/useObserver'
 import useSearchCharacters from '../hooks/useSearchCharacters'
 
 export default function SearchPage() {
-  const ref = useRef(null)
-  const isIntersecting = useObserver(ref)
+  // const ref = useRef(null)
+  // const isIntersecting = useObserver(ref)
 
   const query = qs.decode(location.search.replace('?', ''))
   const { results, loading, maxPages } = useSearchCharacters(query)
 
+  if (loading) {
+    return <p>loading...</p>
+  }
+
+  console.log(results)
+
   return (
     <>
       <h1>Resultados de búsqueda</h1>
-      {<ListOfCharacters data={results} />}
+      {results.map((result) => (
+        <p key={`${result.id}${result.name}`}>{result.name}</p>
+      ))}
     </>
   )
 }
