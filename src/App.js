43fac0a42c163fb0dc5eb146a4b7287f0383a59c
Camilo Paez <cepn26@gmail.com>
2021-03-19 17:13:57 -0500

    :adhesive_bandage: Now Navbar isn't on Lazy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import { Route, Switch } from 'react-router-dom'
 import React from 'react'
-import ScrollToTop from 'components/ScrollToTop'
+
+import { Route, Switch } from 'react-router-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
+
+import ScrollToTop from 'components/ScrollToTop'
+import Navbar from 'components/Navbar'
 
 const CharactersPage = React.lazy(() => import('pages/CharactersPage'))
 const CharacterDetail = React.lazy(() => import('pages/CharacterDetail'))
 const SearchPage = React.lazy(() => import('pages/SearchPage'))
-const Navbar = React.lazy(() => import('components/Navbar'))
 const HomePage = React.lazy(() => import('pages/HomePage'))
 const Error404 = React.lazy(() => import('pages/Error404'))
 
