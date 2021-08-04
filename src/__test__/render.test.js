5b749deab712d915744c6d581670185ab77492b1
Camilo Paez <cepn26@gmail.com>
2021-08-03 19:08:52 -0500

    bump dependencies

--- src/__test__/render.test.js
+++ src/__test__/render.test.js
@@ -5,6 +5,8 @@ import '@testing-library/jest-dom/extend-expect'
 
 import App from '../App'
 
+// someday i will add more test...
+
 test('<App /> renders', async () => {
   const renderApp = render(
     <Suspense fallback={<p>Hola</p>}>
